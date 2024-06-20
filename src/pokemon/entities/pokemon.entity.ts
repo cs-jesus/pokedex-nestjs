745ced420fcd0f2fb1bde056318c9198fb4610d8
Caue Jesus <cs.jesus@gmail.com>
2024-06-20 13:41:27 -0300

    Create and Read HTTP request

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,9 @@
-export class Pokemon {}
+import { Prisma } from "@prisma/client";
+
+export class Pokemon implements Prisma.PokemonUncheckedCreateInput {
+    id?: number;
+    name: string;
+    height?: number;
+    images?: Prisma.ImageUncheckedCreateNestedManyWithoutPokemonInput;
+}
+
